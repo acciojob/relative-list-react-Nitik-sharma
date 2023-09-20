31272e31fa173b1ff5821eafad9cc0ad2ce75e6f
Nitik sharma <sharmanitik36@gmail.com>
2023-09-21 00:00:40 +0530

    submitting code-Thu Sep 21 2023 00:00:38 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  const relative=["Arya",
-  "Sahana",
-  "Rohan",
-  "Kavya",
-  "Arjun",
-  "Asha",
-  "Vikram",
-  "Sarika",
-  "Pranav",
-  "Meera"];
+  const relative=[ { id: 1, name: "Arya" },
+  { id: 2, name: "Sahana" },
+  { id: 3, name: "Rohan" },
+  { id: 4, name: "Kavya" },
+  { id: 5, name: "Arjun" },
+  { id: 6, name: "Asha" },
+  { id: 7, name: "Vikram" },
+  { id: 8, name: "Sarika" },
+  { id: 9, name: "Pranav" },
+  { id: 10, name: "Meera" }];
+  
   console.log(relative)
   return (
     <div id="main">
       <h1>Hello</h1>
            <ol>
            {
-            relative.map((per,index)=>(
-              <li key={index}>{per}</li>
+            relative.map((per)=>(
+              <li key={per.id}>{per.name}</li>
             ))
            }
             </ol>   
