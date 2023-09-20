fc5cfa233aa1a15206c28174aa7d63c69c9db52a
Nitik sharma <sharmanitik36@gmail.com>
2023-09-20 23:53:52 +0530

    submitting code-Wed Sep 20 2023 23:53:50 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const App = () => {
+  const relative=["Arya",
+  "Sahana",
+  "Rohan",
+  "Kavya",
+  "Arjun",
+  "Asha",
+  "Vikram",
+  "Sarika",
+  "Pranav",
+  "Meera"];
+  console.log(relative)
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <h1>Hello</h1>
+           <ol>
+           {
+            relative.map((per,index)=>(
+              <li key={index}>{per}</li>
+            ))
+           }
+            </ol>   
     </div>
   )
 }
